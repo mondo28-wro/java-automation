55a453cdd7fe7155698bdea54b0502509a24e107
mondo28-wro <tomekpawelski@gmail.com>
2023-10-20 09:57:55 +0200

    reverse int

--- Intro/src/Exercises02/ReverseInt.java
+++ Intro/src/Exercises02/ReverseInt.java
@@ -8,18 +8,18 @@ For example, if the int is 15423, the output shall be "3 2 4 5 1", with a space 
 public class ReverseInt {
 
 	static int reverse(int n) {
-		// reversed number
 		int rev = 0;
-		// remainder
 		int rem;
 
 		while (n > 0) {
 			rem = n % 10;
 			rev = (rev * 10) + rem;
 			n = n / 10;
+			
 		}
-
+			
 		return rev;
+		
 	}
 
 	// Driver Function
